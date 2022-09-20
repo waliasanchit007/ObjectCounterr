0a838a5e911047e42afa4f60bbb74ad449fe7cec
Sanchit Walia <swalia_be18@thapar.edu>
2022-09-21 03:49:03 +0530

    initial working commit

---
+++ app/src/main/java/com/example/objectcounter/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.objectcounter
+
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.objectcounter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        supportActionBar?.hide()
+    }
+
+    override fun onBackPressed() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            // Workaround for Android Q memory leak issue in IRequestFinishCallback$Stub.
+            // (https://issuetracker.google.com/issues/139738913)
+            finishAfterTransition()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/objectcounter/OverlayView.kt
@@ -0,0 +1,101 @@
+package com.example.objectcounter
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.ContextCompat
+import org.tensorflow.lite.task.vision.detector.Detection
+import java.util.*
+import kotlin.math.max
+
+class OverlayView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private var results: List<Detection> = LinkedList<Detection>()
+    private var boxPaint = Paint()
+    private var textBackgroundPaint = Paint()
+    private var textPaint = Paint()
+
+    private var scaleFactor: Float = 1f
+
+    var b: Map<String, Int> = mapOf()
+    private var bounds = Rect()
+
+    init {
+        initPaints()
+    }
+
+    fun clear() {
+        textPaint.reset()
+        textBackgroundPaint.reset()
+        boxPaint.reset()
+        invalidate()
+        initPaints()
+    }
+
+    private fun initPaints() {
+        textBackgroundPaint.color = Color.BLACK
+        textBackgroundPaint.style = Paint.Style.FILL
+        textBackgroundPaint.textSize = 50f
+
+        textPaint.color = Color.WHITE
+        textPaint.style = Paint.Style.FILL
+        textPaint.textSize = 50f
+
+        boxPaint.color = ContextCompat.getColor(context!!, R.color.bounding_box_color)
+        boxPaint.strokeWidth = 8F
+        boxPaint.style = Paint.Style.STROKE
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        for (result in results) {
+            val boundingBox = result.boundingBox
+
+            val top = boundingBox.top * scaleFactor
+            val bottom = boundingBox.bottom * scaleFactor
+            val left = boundingBox.left * scaleFactor
+            val right = boundingBox.right * scaleFactor
+
+            // Draw bounding box around detected objects
+            val drawableRect = RectF(left, top, right, bottom)
+            canvas.drawRect(drawableRect, boxPaint)
+
+            // Create text to display alongside detected objects
+            val drawableText =
+                result.categories[0].label + " " +
+                        String.format("%.2f", result.categories[0].score)
+            // Draw rect behind display text
+            textBackgroundPaint.getTextBounds(drawableText, 0, drawableText.length, bounds)
+            val textWidth = bounds.width()
+            val textHeight = bounds.height()
+            canvas.drawRect(
+                left,
+                top,
+                left + textWidth + BOUNDING_RECT_TEXT_PADDING,
+                top + textHeight + BOUNDING_RECT_TEXT_PADDING,
+                textBackgroundPaint
+            )
+
+            // Draw text for detected object
+            canvas.drawText(drawableText, left, top + bounds.height(), textPaint)
+        }
+        canvas.drawText(b.toString(), 30f, 60f, textPaint)
+    }
+
+    fun setResults(
+        detectionResults: MutableList<Detection>,
+        imageHeight: Int,
+        imageWidth: Int,
+    ) {
+        results = detectionResults
+        val a = mutableListOf<String>()
+        results.forEach { a.add(it.categories[0].label) }
+        b = a.groupingBy { it }.eachCount()
+        scaleFactor = max(width * 1f / imageWidth, height * 1f / imageHeight)
+    }
+
+    companion object {
+        private const val BOUNDING_RECT_TEXT_PADDING = 8
+    }
+}
